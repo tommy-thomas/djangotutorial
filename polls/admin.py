0d5fd32169961b439f9d5338fc627b7f3efb286d
tommy-thomas <tommyt67@gmail.com>
2025-10-06 19:35:53 -0500

    customize admin, untrack files

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+# Register your models here.
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Question", {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"], "classes": ["collapse"]}),
+    ]
+
+    list_display = ["question_text", "pub_date", "was_published_recently"]
+
+    list_filter = ["pub_date"]
+
+    search_fields = ["question_text"]
+    
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
